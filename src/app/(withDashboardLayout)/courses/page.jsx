b56b5c2504f4a21474f8994962809137f3c45481
jubayer718 <jubayerahmed2462@gmail.com>
2026-02-18 22:02:43 +0600

    some build issue

--- src/app/(withDashboardLayout)/courses/page.jsx
+++ src/app/(withDashboardLayout)/courses/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import useAxiosPublic from '@/app/hooks/useAxiosPublic';
 import Navbar from '@/components/layout/Navbar';
 import { Button } from '@/components/ui/button';
